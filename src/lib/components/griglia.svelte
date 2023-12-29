e587872e8ff5705f49e49bec86a7d17692827909
MarcoCellini <marco.cellini@istitutoagnelli.it>
2023-12-29 16:22:44 +0100

    cellini: initialization of movement

--- src/lib/components/griglia.svelte
+++ src/lib/components/griglia.svelte
@@ -1,33 +1,103 @@
 <script>
-    let list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13 ,14 ,15]
+	function getRandomInt(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+	}
+
+	function motion(direction) {
+		console.log(direction);
+		for (let i = 0; i < 16; i++) {
+			switch (direction) {
+				case 'left':
+					left(i);
+					break;
+
+				case 'right':
+					right(i);
+					break;
+
+				case 'up':
+					up(i);
+					break;
+
+				case 'down':
+					down(i);
+					break;
+
+				default:
+					break;
+			}
+		}
+	}
+
+	function left(i) {
+		if (list[i] != '') {
+			list[Math.floor(i / 4) * 4] = list[i];
+			Math.floor(i / 4) * 4 != i ? (list[i] = '') : null;
+		}
+	}
+
+	function right(i) {
+		if (list[i] != '') {
+			list[Math.floor(i / 4) * 4 + 3] = list[i];
+			Math.floor(i / 4) * 4 + 3 != i ? (list[i] = '') : null;
+		}
+	}
+
+	function up(i) {
+		if (list[i] != '') {
+			list[i - Math.floor(i / 4) * 4] = list[i];
+			i - Math.floor(i / 4) * 4 != i ? (list[i] = '') : null;
+		}
+	}
+
+	function down(i) {
+		if (list[i] != '') {
+			list[12 + i - Math.floor(i / 4) * 4] = list[i];
+			12 + i - Math.floor(i / 4) * 4 != i ? (list[i] = '') : null;
+		}
+	}
+
+	let list = [];
+	/* let dir = 'NEUTRAL'; */
+
+	/* initialization of the page */
+	for (let i = 0; i < 16; i++) list.push('');
+	list[getRandomInt(0, 16)] = '2';
 </script>
 
 <div class="griglia">
-    {#each list as l}
-        <div class="casella">{l}</div>
-    {/each}
+	{#each list as l}
+		<div class="casella" style="background-color: var(--n{l}-color);">{l}</div>
+	{/each}
 </div>
 
+<input type="button" value="left" on:click={() => motion('left')} />
+<input type="button" value="right" on:click={() => motion('right')} />
+<input type="button" value="up" on:click={() => motion('up')} />
+<input type="button" value="down" on:click={() => motion('down')} />
+
 <style>
-    .griglia{
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr 1fr;
-        grid-template-rows: 1fr 1fr 1fr 1fr;
-        height: 700px;
-        width: 700px;
-        margin: auto;
-        padding: 10px;
-        background-color: var(--bg-color);
-        border-radius: 20px;
-    }
-    .casella{
-        height: 150px;
-        width: 150px;
-        background-color: var(--cs-color);
-        margin: 10px;
-        border-radius: 10px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+	.griglia {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr 1fr;
+		grid-template-rows: 1fr 1fr 1fr 1fr;
+		height: 700px;
+		width: 700px;
+		margin: auto;
+		padding: 10px;
+		background-color: var(--bg-color);
+		border-radius: 20px;
+	}
+	.casella {
+		height: 150px;
+		width: 150px;
+		/* background-color: var(--n-color); */
+		margin: 10px;
+		border-radius: 10px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,25 @@
 <script>
-    import Griglia from "../lib/components/griglia.svelte";
+	import Griglia from '../lib/components/griglia.svelte';
 </script>
 
 <div>
-    <Griglia/>
+	<Griglia />
 </div>
 
 <style>
-    :global(:root){
-        --bg-color: grey;
-        --cs-color: lightgrey;
-        --n2-color: white;
-        --n4-color: beige;
-        --n8-color: yellow;
-        --n16-color: orange;
-        --n32-color: red;
-        --n64-color: violet;
-        --n128-color: purple;
-        --n256-color: lightblue;
-        --n512-color: blue;
-        --n1024-color: lightgreen;
-        --n2048-color: green;
-        }
+	:global(:root) {
+		--bg-color: grey;
+		--n-color: rgb(195, 195, 195);
+		--n2-color: white;
+		--n4-color: beige;
+		--n8-color: yellow;
+		--n16-color: orange;
+		--n32-color: red;
+		--n64-color: violet;
+		--n128-color: purple;
+		--n256-color: lightblue;
+		--n512-color: blue;
+		--n1024-color: lightgreen;
+		--n2048-color: green;
+	}
 </style>

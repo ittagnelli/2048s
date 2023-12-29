58b9099fc9d1add7bf853bf04e1d634108d5b6e0
Marco-Minetti <marco.minetti@istitutoagnelli.it>
2023-12-29 19:57:24 +0100

    ultimate update

---
+++ src/components/title.svelte
@@ -0,0 +1,34 @@
+<script>
+    import Score from "../components/score.svelte";
+</script>
+
+<div class="sub-container">
+    <div class="name">2048</div> 
+    <Score name="RECORD"/>
+</div>
+
+<hr>
+
+<div class="sub-container">
+    <div style="margin-top: 4vh;">Join the numbers and get to the <b>2048 tile!</b></div>
+    <Score name="SCORE" adj=1/>
+</div>
+
+<style>
+    .name {
+        font-size: 10vh;
+        font-weight: bolder;
+    }
+
+    .sub-container  {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        border: 1px solid red;
+    }
+
+    hr {
+        height: 1vh;
+        background-color: peru;
+        border: 1px double black;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script> 
 
     import Griglia from "../components/griglia.svelte";
-    import Score from "../components/score.svelte";
     import Restart from "../components/restart.svelte";
+	import Title from "../components/title.svelte";
 
     import {score} from "../js/store";
 
@@ -18,17 +18,8 @@
 </script>
 
 <div class="container">
-    <div class="sub-container">
-        <div class="name">2048</div> 
-        <Score name="RECORD"/>
-    </div>
+    <Title/>
 
-    <hr>
-
-    <div class="sub-container">
-        <div style="margin-top: 4vh;">Join the numbers and get to the <b>2048 tile!</b></div>
-        <Score name="SCORE" adj=1/>
-    </div>
     <div class="table">
         <Griglia/>
     </div>
@@ -54,28 +45,11 @@
         --n2048-color: green;
     }
 
-    hr {
-        height: 1vh;
-        background-color: peru;
-        border: 1px double black;
-    }
-
     .container {
         width: 50%;
         margin: 0 auto;
         text-align: center;
         height: min-content;
-        border: 1px solid red;
-    }
-
-    .name {
-        font-size: 10vh;
-        font-weight: bolder;
-    }
-
-    .sub-container  {
-        display: grid;
-        grid-template-columns: 1fr 1fr;
         border: 1px solid red;
     }
 

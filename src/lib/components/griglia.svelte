5864ce529ec03687901718856802a78032e5fb27
MarcoCellini <marco.cellini@istitutoagnelli.it>
2023-12-30 13:11:00 +0100

    cellini: adjust logic of movement and insert join of equal cells

--- src/lib/components/griglia.svelte
+++ src/lib/components/griglia.svelte
@@ -1,70 +1,79 @@
 <script>
 	function getRandomInt(min, max) {
+		/* random number in a range >= n && < m */
 		min = Math.ceil(min);
 		max = Math.floor(max);
 		return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
 	}
 
+	function move(i, final_index) {
+		/* general function to move cells using initial index and final index of the cell */
+		list[final_index] = list[i];
+		final_index != i ? (list[i] = '') : null;
+	}
+
+	function join(a, b) {
+		console.log("sono uguali " + a + " e " + b);
+		list[b] = String(list[a] * 2);
+		list[a] = '';
+	}
+
+	/* function control_left(initial_index, final_index) {
+		console.log(initial_index, final_index);
+
+		for (let i = initial_index - 1; i >= final_index; i--) {
+			if (list[i] !== list[initial_index] && list[i] !== '') break;
+			list[i] === list[initial_index] ? join(i, initial_index) : null;
+		}
+
+		console.log(list);
+		//move(initial_index, final_index);
+	} */
+
+	function control(initial_index, final_index, increment) {
+		console.log(initial_index, final_index);
+
+		for (let i = initial_index + increment; i != final_index + increment; i += increment) {
+			console.log(i);
+			if (list[i] !== list[initial_index] && list[i] !== '') break;
+			list[i] === list[initial_index] ? join(i, initial_index) : null;
+		}
+
+		move(initial_index, final_index);
+	}
+
 	function motion(direction) {
-		console.log(direction);
-		for (let i = 0; i < 16; i++) {
+		/* selector of the type of movement, it will be deal with the event created by component input */
+		for (let i = 0; i < 16; i++)
 			switch (direction) {
 				case 'left':
-					left(i);
+					list[i] != '' ? control(i, Math.floor(i / 4) * 4, -1) : null;
 					break;
 
 				case 'right':
-					right(i);
+					list[i] != '' ? control(i, Math.floor(i / 4) * 4 + 3, 1) : null;
 					break;
 
 				case 'up':
-					up(i);
+					list[i] != '' ? control(i, i - Math.floor(i / 4) * 4) : null;
 					break;
 
 				case 'down':
-					down(i);
+					list[i] != '' ? control(i, 12 + i - Math.floor(i / 4) * 4) : null;
 					break;
 
 				default:
 					break;
 			}
-		}
-	}
-
-	function left(i) {
-		if (list[i] != '') {
-			list[Math.floor(i / 4) * 4] = list[i];
-			Math.floor(i / 4) * 4 != i ? (list[i] = '') : null;
-		}
-	}
-
-	function right(i) {
-		if (list[i] != '') {
-			list[Math.floor(i / 4) * 4 + 3] = list[i];
-			Math.floor(i / 4) * 4 + 3 != i ? (list[i] = '') : null;
-		}
-	}
-
-	function up(i) {
-		if (list[i] != '') {
-			list[i - Math.floor(i / 4) * 4] = list[i];
-			i - Math.floor(i / 4) * 4 != i ? (list[i] = '') : null;
-		}
-	}
-
-	function down(i) {
-		if (list[i] != '') {
-			list[12 + i - Math.floor(i / 4) * 4] = list[i];
-			12 + i - Math.floor(i / 4) * 4 != i ? (list[i] = '') : null;
-		}
 	}
 
 	let list = [];
-	/* let dir = 'NEUTRAL'; */
 
 	/* initialization of the page */
 	for (let i = 0; i < 16; i++) list.push('');
-	list[getRandomInt(0, 16)] = '2';
+	let random = getRandomInt(0, 16);
+	list[random] = '2';
+	list[random + 2] = '2';
 </script>
 
 <div class="griglia">
@@ -73,6 +82,7 @@
 	{/each}
 </div>
 
+<!-- temporary button to create the movement -->
 <input type="button" value="left" on:click={() => motion('left')} />
 <input type="button" value="right" on:click={() => motion('right')} />
 <input type="button" value="up" on:click={() => motion('up')} />
